47a6b2b4613f5927fe0c00d02497e19ff7ab6571
Attsu-dev <atsushi.pvt@gmail.com>
2025-07-23 11:36:44 +0900

    自作関数をpriveteに

--- group6.h
+++ group6.h
@@ -25,30 +25,6 @@ class Group6 : public Player {
   GameStatus gamestatus;
   CardSet pile;
 
- public:
-  Group6(const char* name = "Group6") : Player(name) {
-    // JOKERの初期化
-    JOKER.set(Card::SUIT_JOKER, Card::RANK_JOKER);
-    // rank_setの初期化
-    for (int num = 1; num <= 13; num++) {
-      for (int suit = 1; suit <= 4; suit++) {
-        rank_set[num].insert(Card(suit, num));
-      }
-    }
-    rank_set[Card::RANK_JOKER].insert(JOKER);
-  }
-  ~Group6() {}
-
-  // 思考処理の関数：このクラスで実装する
-  void ready(void);
-  bool follow(const GameStatus&, CardSet&);
-  bool approve(const GameStatus&);
-
-  // 比較関数(ソート用)
-  static bool myCardCmp(const Card& a, const Card& b) {
-    return a.strength() <= b.strength();
-  }
-
   /*------------------ 自作ツール ---------------------*/
   // CardSetの中で、数字がrankのカードの集合を返す
   CardSet filterByRank(const CardSet& cs, int rank);
@@ -73,6 +49,30 @@ class Group6 : public Player {
 
   // カードセットのポイントを返す
   int getPoint(const CardSet& cs);
+
+ public:
+  Group6(const char* name = "Group6") : Player(name) {
+    // JOKERの初期化
+    JOKER.set(Card::SUIT_JOKER, Card::RANK_JOKER);
+    // rank_setの初期化
+    for (int num = 1; num <= 13; num++) {
+      for (int suit = 1; suit <= 4; suit++) {
+        rank_set[num].insert(Card(suit, num));
+      }
+    }
+    rank_set[Card::RANK_JOKER].insert(JOKER);
+  }
+  ~Group6() {}
+
+  // 思考処理の関数：このクラスで実装する
+  void ready(void);
+  bool follow(const GameStatus&, CardSet&);
+  bool approve(const GameStatus&);
+
+  // 比較関数(ソート用)
+  static bool myCardCmp(const Card& a, const Card& b) {
+    return a.strength() <= b.strength();
+  }
 };
 
 #endif
